9290b35b3e27cf592939642c47afbcfa1eac49ef
Fernando G <kev9504@gmail.com>
2018-11-16 20:18:12 +0000

    intalled pending dependencies, completed test for bug fix related to gear

--- src/common/header/Header.test.js
+++ src/common/header/Header.test.js
@@ -1,9 +1,22 @@
+/* eslint-env jest */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Header';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Drawer from '@material-ui/core/Drawer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Header />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+configure({adapter: new Adapter()});
+describe('<Header />', ()=>{
+  const wrapper=shallow(<Header />);
+  it('should render without crashing', ()=>{
+    expect(wrapper.find('#page-header')).toHaveLength(1);
+  });
+  it('should not render drawer when drawerIsOpened==false',()=>{
+    expect(wrapper.find(<Drawer />)).toHaveLength(0);
+  });
+  it('should render drawer when drawerIsOpened==true',()=>{
+    wrapper.setState({drawerIsOpened: true});
+    expect(wrapper.find(Drawer)).toHaveLength(1);
+  });
+})

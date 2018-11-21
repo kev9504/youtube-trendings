2683e2f8bc47b15d1d3f30ee5fd2d2bc297edf40
Fernando G <kev9504@gmail.com>
2018-11-21 03:20:35 +0000

    tested youtube api error handling

--- src/components/youtube/player/Youtube.Player.jsx
+++ src/components/youtube/player/Youtube.Player.jsx
@@ -1,3 +1,4 @@
+/*eslint-disable*/
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import './Youtube.Player.scss';
@@ -9,22 +10,25 @@ class YoutubePlayer extends Component {
     const id = window.location.href
       .replace(/^.*\//g, '')
       .replace(/^.*\..*/g, '');
-    const iframe = `<iframe title="Video" 
-    width="100%" height="100%" 
-    src="https://www.youtube.com/embed/${id}?autoplay=1" 
-    frameBorder="0" allowFullScreen/>`;
+    const iframe = `<iframe 
+    title="Video" 
+    width="100%" 
+    height="100%"
+    id="enbded-frame"
+    src="https://www.youtube.com/embed/${id}?autoplay=1"
+    frameBorder="0" 
+    allowFullScreen/>`;
+
     setTimeout(() => {
-      if (document.getElementsByClassName('frame-block')[0]) {
+      if(document.getElementsByClassName('frame-block')[0]) {
         document.getElementsByClassName('frame-block')[0].innerHTML = iframe;
       }
     }, 1000);
-
   }
-
   render() {
     return (
       <div className="video-container">
-        <div className="frame-block"/>
+        <div className="frame-block" id="v-frame"/>
         <div className="controls">
           <Link className="btn btn-primary" to="/youtube"> &#60; Back to Trends</Link>
         </div>
